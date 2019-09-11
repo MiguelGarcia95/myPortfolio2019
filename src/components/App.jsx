c05b8d0a2f4df6301dc1714c4c24f28cba5ed30a
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-11 16:35:24 -0500

    style content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,16 +14,20 @@ function App() {
               <img className="background" src="https://www.cssauthor.com/wp-content/uploads/2014/08/Boarding-pass-app-ui-psd.jpg" alt=""/>
             </div>
             <div className="project flex-1">
-              <img className='background' src="https://cssauthor.com/wp-content/uploads/2014/08/App-News-iPhone-6-PSD-1-289x576.jpg" alt=""/>
+              <img className='background' src="https://appsamurai.com/wp-content/uploads/2017/08/2-2.png" alt=""/>
             </div>
             <div className="project pl flex-2">
               <img className='background' src="https://appsamurai.com/wp-content/uploads/2017/08/2-2.png" alt=""/>
             </div>
           </div>
           <div className="row">
-            <div className="project pf flex-3"></div>
+            <div className="project pf flex-3">
+              <img className="background" src="https://www.cssauthor.com/wp-content/uploads/2014/08/Boarding-pass-app-ui-psd.jpg" alt=""/>
+            </div>
             {/* <div className="project flex-2"></div> */}
-            <div className="project pl flex-1"></div>
+            <div className="project pl flex-1">
+              <img className='background' src="https://files.yande.re/image/3f68443ebd80ed91d6cc080cf9a26f16/yande.re%20286316%20arisato_minato%20headphones%20male%20megaten%20persona%20persona_3%20sogabe_shuuji.jpg" alt=""/>
+            </div>
           </div>
           {/* <div className="project">
             <div className="images">
