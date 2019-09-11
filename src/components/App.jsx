27abea56dcf3a69e1014c3dafee382ee6294abcb
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-11 13:15:04 -0500

    style content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,9 +10,11 @@ function App() {
       <div className="content">
         <div className="section projects">
           <div className="row">
-
+            <div className="project pf flex-1"></div>
+            <div className="project flex-2"></div>
+            <div className="project pl flex-1"></div>
           </div>
-          <div className="project">
+          {/* <div className="project">
             <div className="images">
               <img className="back" src="https://files.yande.re/sample/3f68443ebd80ed91d6cc080cf9a26f16/yande.re%20286316%20sample%20arisato_minato%20headphones%20male%20megaten%20persona%20persona_3%20sogabe_shuuji.jpg" alt="" />
               <img className="front" src="https://files.yande.re/sample/3f68443ebd80ed91d6cc080cf9a26f16/yande.re%20286316%20sample%20arisato_minato%20headphones%20male%20megaten%20persona%20persona_3%20sogabe_shuuji.jpg" alt="" />
@@ -21,7 +23,7 @@ function App() {
               <h1 className="title"></h1>
               <p className="description"></p>
             </div>
-          </div>
+          </div> */}
         </div>
         <div className="section skills"></div>
         <div className="section about"></div>
