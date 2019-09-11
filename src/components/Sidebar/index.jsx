f6c3ad518d362369a2f1a998cbaf9ce892b0eb1a
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-11 16:48:38 -0500

    style content

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -3,17 +3,22 @@ import './styles/style.css';
 
 export default function Sidebar() {
   return (
-    <header className="sidebar">
-      <nav>
-        <p>Projects</p>
-        <p>Skills</p>
-        <p>About</p>
-        <p>Contact</p>
-      </nav>
-      <div className="name">
-        <h1>Miguel Garcia</h1>
-        <h2>Web Developer</h2>
+    <React.Fragment>
+      <header className="sidebar">
+        <nav>
+          <p>Projects</p>
+          <p>Skills</p>
+          <p>About</p>
+          <p>Contact</p>
+        </nav>
+        <div className="name">
+          <h1>Miguel Garcia</h1>
+          <h2>Web Developer</h2>
+        </div>
+      </header>
+      <div className="nav-name">
+        <h1>Projects</h1>
       </div>
-    </header>
+    </React.Fragment>
   )
 }
