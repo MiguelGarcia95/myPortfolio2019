3d106885a8e6af3b2d595237378b87d5af9a07b0
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-12 14:44:27 -0500

    create skill and about content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -57,9 +57,24 @@ function App() {
             />
           </div>
         </div>
-        <div className="section skills"></div>
-        <div className="section about"></div>
-        <div className="section contact"></div>
+        <div className="section skills">
+          <div className="skill-row">
+            <div className="skill">
+              <div className="skill-icon"></div>
+              <div className="skill-name"></div>
+            </div>
+          </div>
+        </div>
+        <div className="section about">
+          <div className="about">
+            <div className="about-decal"></div>
+            <div className="about-content"></div>
+          </div>
+        </div>
+        <div className="section contact">
+          <div className="contact-title"></div>
+          <div className="contact-text"></div>
+        </div>
       </div>
     </div>
   );
