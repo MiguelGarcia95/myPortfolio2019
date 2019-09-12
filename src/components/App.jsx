f32a52e50b1708d8e7b4f5137f6b5eb8d135d5cd
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-11 21:25:15 -0500

    style content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,22 +13,19 @@ function App() {
             <div className="project pf flex-1">
               <img className="background" src="https://www.cssauthor.com/wp-content/uploads/2014/08/Boarding-pass-app-ui-psd.jpg" alt=""/>
               <div className="info">
-                <h1 className="title"></h1>
-                <p className="description"></p>
+                <h1 className="title">Title Here</h1>
               </div>
             </div>
             <div className="project flex-1">
               <img className='background' src="https://appsamurai.com/wp-content/uploads/2017/08/2-2.png" alt=""/>
               <div className="info">
-                <h1 className="title"></h1>
-                <p className="description"></p>
+                <h1 className="title">Title Here</h1>
               </div>
             </div>
             <div className="project pl flex-2">
               <img className='background' src="https://appsamurai.com/wp-content/uploads/2017/08/2-2.png" alt=""/>
               <div className="info">
-                <h1 className="title"></h1>
-                <p className="description"></p>
+                <h1 className="title">Title Here</h1>
               </div>
             </div>
           </div>
@@ -36,16 +33,14 @@ function App() {
             <div className="project pf flex-3">
               <img className="background" src="https://www.cssauthor.com/wp-content/uploads/2014/08/Boarding-pass-app-ui-psd.jpg" alt=""/>
               <div className="info">
-                <h1 className="title"></h1>
-                <p className="description"></p>
+                <h1 className="title">Title Here</h1>
               </div>
             </div>
             {/* <div className="project flex-2"></div> */}
             <div className="project pl flex-1">
               <img className='background' src="https://files.yande.re/image/3f68443ebd80ed91d6cc080cf9a26f16/yande.re%20286316%20arisato_minato%20headphones%20male%20megaten%20persona%20persona_3%20sogabe_shuuji.jpg" alt=""/>
               <div className="info">
-                <h1 className="title"></h1>
-                <p className="description"></p>
+                <h1 className="title">Title Here</h1>
               </div>
             </div>
           </div>
