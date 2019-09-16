ac1ba0655b62e75786efded1a174b85729dda04c
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-16 11:46:25 -0500

    style projects

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -22,43 +22,38 @@ class Sidebar extends React.Component {
   render() {
     const {currentSection} = this.props;
     return (
-      <React.Fragment>
-        <header className="sidebar">
-          <nav>
-            <p 
-              className={this.isActive(currentSection, 'projects') ? 'active' : ''} 
-              onClick={() => this.onNavClick('projects', currentSection)}
-            >
-              Projects
-            </p>
-            <p 
-              className={this.isActive(currentSection, 'skills') ? 'active' : ''} 
-              onClick={() => this.onNavClick('skills', currentSection)} 
-            >
-              Skills
-            </p>
-            <p 
-              className={this.isActive(currentSection, 'about') ? 'active' : ''} 
-              onClick={() => this.onNavClick('about', currentSection)} 
-            >
-              About
-            </p>
-            <p 
-              className={this.isActive(currentSection, 'contact') ? 'active' : ''} 
-              onClick={() => this.onNavClick('contact', currentSection)} 
-            >
-              Contact
-            </p>
-          </nav>
-          <div className="name">
-            <h1>Miguel Garcia</h1>
-            <h2>Web Developer</h2>
-          </div>
-        </header>
-        <div className="nav-name">
-          {/* <h1>Projects</h1> */}
+      <header className="sidebar">
+        <nav>
+          <p 
+            className={this.isActive(currentSection, 'projects') ? 'active' : ''} 
+            onClick={() => this.onNavClick('projects', currentSection)}
+          >
+            Projects
+          </p>
+          <p 
+            className={this.isActive(currentSection, 'skills') ? 'active' : ''} 
+            onClick={() => this.onNavClick('skills', currentSection)} 
+          >
+            Skills
+          </p>
+          <p 
+            className={this.isActive(currentSection, 'about') ? 'active' : ''} 
+            onClick={() => this.onNavClick('about', currentSection)} 
+          >
+            About
+          </p>
+          <p 
+            className={this.isActive(currentSection, 'contact') ? 'active' : ''} 
+            onClick={() => this.onNavClick('contact', currentSection)} 
+          >
+            Contact
+          </p>
+        </nav>
+        <div className="name">
+          <h1>Miguel Garcia</h1>
+          <h2>Web Developer</h2>
         </div>
-      </React.Fragment>
+      </header>
     )
   }
 }
