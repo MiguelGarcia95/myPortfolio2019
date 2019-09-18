7b385681a5f9ef06a19ace232361372179fe5a6e
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-18 14:39:10 -0500

    style project display

--- src/components/ProjectDisplay/index.jsx
+++ src/components/ProjectDisplay/index.jsx
@@ -5,11 +5,17 @@ export default function ProjectDisplay({project}) {
   return (
     <div className='section project-display startPos'>
       <div className="project-images">
-        <div className="first-image">
-          <img src="" alt=""/>
+        <div className="project-image first-image">
+          <img src="https://gameranx.com/wp-content/uploads/2016/09/Nier-Automata-1080P-Wallpaper-1.jpg" alt=""/>
         </div>
-        <div className="second-image">
-          <img src="" alt=""/>
+        <div className="project-image second-image">
+          <img src="https://i.redd.it/1dbjwhvpoia11.jpg" alt=""/>
+        </div>
+        <div className="project-image third-image">
+          <img src="https://i.pinimg.com/736x/bf/b4/ec/bfb4ec55cd5f515f267b918b36b18b47.jpg" alt=""/>
+        </div>
+        <div className="project-image fourth-image">
+          <img src="https://www.pcgamesn.com/wp-content/uploads/2018/08/Cyberpunk-2077-screenshot1.jpg" alt=""/>
         </div>
       </div>
       <div className="project-content">
