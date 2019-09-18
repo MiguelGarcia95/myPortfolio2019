4bc1881973d2120c4431c5e30b1c75b421534e1b
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-18 11:44:21 -0500

    style project display

--- src/components/ProjectDisplay/index.jsx
+++ src/components/ProjectDisplay/index.jsx
@@ -4,6 +4,22 @@ import './styles/style.css';
 export default function ProjectDisplay({project}) {
   return (
     <div className='section project-display startPos'>
+      <div className="project-images">
+        <div className="first-image">
+          <img src="" alt=""/>
+        </div>
+        <div className="second-image">
+          <img src="" alt=""/>
+        </div>
+      </div>
+      <div className="project-content">
+        <div className="description">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum molestias ullam dolores sunt explicabo labore aspernatur quam ipsa consequatur doloribus omnis vitae veritatis, beatae fugiat autem vel est enim ratione.</p>
+        </div>
+        <div className="used">
+          <p></p>
+        </div>
+      </div>
       {project}
     </div>
   )
