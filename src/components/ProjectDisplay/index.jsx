d73d64cef349f33c87ff6f7449242e7da9d1c683
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-18 22:40:01 -0500

    style project display

--- src/components/ProjectDisplay/index.jsx
+++ src/components/ProjectDisplay/index.jsx
@@ -6,6 +6,26 @@ const onReturnClick = (sectionLeaves, sectionEnters) => {
   sectionEnters('projects');
 }
 
+const IMAGES = {
+  imageCount: 4,
+  images: [
+    'https://gameranx.com/wp-content/uploads/2016/09/Nier-Automata-1080P-Wallpaper-1.jpg',
+    'https://i.redd.it/1dbjwhvpoia11.jpg',
+    'https://i.pinimg.com/736x/bf/b4/ec/bfb4ec55cd5f515f267b918b36b18b47.jpg',
+    'https://www.pcgamesn.com/wp-content/uploads/2018/08/Cyberpunk-2077-screenshot1.jpg'
+  ]
+}
+
+const displayImages = () => {
+  return IMAGES.images.map((image, i) => {
+    return (
+      <div className="project-image first-image" key={i}>
+        <img src={image} alt={image}/>
+      </div>
+    )
+  })
+}
+
 export default function ProjectDisplay({project, sectionLeaves, sectionEnters}) {
   return (
     <div className='section project-display startPos'>
@@ -18,18 +38,7 @@ export default function ProjectDisplay({project, sectionLeaves, sectionEnters}) 
         </div>
       </div>
       <div className="project-images">
-        <div className="project-image first-image">
-          <img src="https://gameranx.com/wp-content/uploads/2016/09/Nier-Automata-1080P-Wallpaper-1.jpg" alt=""/>
-        </div>
-        <div className="project-image second-image">
-          <img src="https://i.redd.it/1dbjwhvpoia11.jpg" alt=""/>
-        </div>
-        <div className="project-image third-image">
-          <img src="https://i.pinimg.com/736x/bf/b4/ec/bfb4ec55cd5f515f267b918b36b18b47.jpg" alt=""/>
-        </div>
-        <div className="project-image fourth-image">
-          <img src="https://www.pcgamesn.com/wp-content/uploads/2018/08/Cyberpunk-2077-screenshot1.jpg" alt=""/>
-        </div>
+        {displayImages()}
       </div>
       <div className="project-content">
         <div className="project-name">
