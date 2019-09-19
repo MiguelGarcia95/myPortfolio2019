59182a3a9a5b754da28ec39b0f226efd330e1bc9
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-19 13:57:10 -0500

    style project display

--- src/components/ImageSlider/index.jsx
+++ src/components/ImageSlider/index.jsx
@@ -8,8 +8,8 @@ class ImageSlider extends React.Component {
       return (
         <div className={`project-image ${image.class}`} key={image.id}>
           <div className="slider-controller">
-            <div className="slide-left" onClick={() => this.onClickLeft(image.class)} ></div>
-            <div className="slide-right" onClick={() => this.onClickRight(image.class)} ></div>
+            <div className="slide-left" onClick={() => this.onClickLeft(image, images)} ></div>
+            <div className="slide-right" onClick={() => this.onClickRight(image, images)} ></div>
           </div>
           <img src={image.url} alt={image.id}/>
         </div>
@@ -17,24 +17,33 @@ class ImageSlider extends React.Component {
     })
   }
 
-  onClickLeft = imageName => {
-    console.log('left', this.props.images.imageCount)
+  onClickLeft = (image, images) => {
+    console.log('left', images.imageCount)
+    console.log('imageId', image.id)
+    if (image.id === 1) {
+      this.imageSlideOut('image-1');
+      this.imageSlideIn(`image-${images.imageCount}`);
+    }
   }
 
-  onClickRight = imageName => {
-    console.log('right', this.props.images.imageCount)
+  onClickRight = (image, images) => {
+    console.log('right', images.imageCount)
+    console.log('imageId', image.id)
+    if (image.id === images.imageCount) {
+      this.imageSlideOut('image-4');
+      this.imageSlideIn(`image-1`);
+    }
   }
 
   imageSlideIn = imageName => {
     let tl = new window.TimelineMax();
-    tl.fromTo(`.${imageName}`, 2, {scale: 1, autoAlpha: 1, left: 0},  {left: '-100%', autoAlpha: 0, scale: 2, ease: window.Elastic.easeInOut.config(1, 0.3)})
-    tl.to(`.${imageName}`, 0, {scale: 1})
+    tl.fromTo(`.${imageName}`, 1, {scale: 0, autoAlpha: 0, left: '100%'},  {left: 0, autoAlpha: 1, scale: 1, ease: window.Elastic.easeInOut.config(1, 0.3)}, '+=0.3')
   }
 
   imageSlideOut = imageName => {
     let tl = new window.TimelineMax();
     tl.fromTo(`.${imageName}`, 2, {scale: 1, autoAlpha: 1, left: 0},  {left: '-100%', autoAlpha: 0, scale: 2, ease: window.Elastic.easeInOut.config(1, 0.3)})
-    tl.to(`.${imageName}`, 0, {scale: 1})
+    // tl.to(`.${imageName}`, 0, {scale: 1})
   }
 
 
