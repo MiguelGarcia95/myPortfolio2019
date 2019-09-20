c81b90d4bfb8b9b34f7560e9b48e114ee58f0934
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-20 00:26:31 -0500

    create project data

--- src/projectData.js
+++ src/projectData.js
@@ -1,7 +1,7 @@
 export default [
   {
-    name: '',
-    description: '',
+    name: 'Game View',
+    description: "A game database website. It includes games, game reviews, game franchises and game characters. It contains over 15,000 games. All powered by the giant bomb API.",
     imageCount: 1,
     images: [
       {
@@ -14,6 +14,108 @@ export default [
       'html', 
       'css'
     ],
-    url: '',
-  }
+    url: 'https://young-thicket-81676.herokuapp.com',
+  },
+  {
+    name: 'Draggable Planner (Trello Clone)',
+    description: "A Trello clone",
+    imageCount: 1,
+    images: [
+      {
+        id: 1,
+        url: '',
+        class: 'image-1',
+      }
+    ],
+    used: [
+      'html', 
+      'css'
+    ],
+    url: 'https://react-draggable-planner.herokuapp.com/#/',
+  },
+  {
+    name: 'Moview Search Base',
+    description: "Moview Website made with moviedb",
+    imageCount: 1,
+    images: [
+      {
+        id: 1,
+        url: '',
+        class: 'image-1',
+      }
+    ],
+    used: [
+      'html', 
+      'css'
+    ],
+    url: 'https://movie-searchbase.firebaseapp.com',
+  },
+  {
+    name: 'Nike Shoe Display',
+    description: "Moview Website made with moviedb",
+    imageCount: 1,
+    images: [
+      {
+        id: 1,
+        url: '',
+        class: 'image-1',
+      }
+    ],
+    used: [
+      'html', 
+      'css'
+    ],
+    url: 'https://miguelgarcia95.github.io/Shoe-Page/',
+  },
+  {
+    name: 'Chamber of Nature - Book Promotion Site',
+    description: "Moview Website made with moviedb",
+    imageCount: 1,
+    images: [
+      {
+        id: 1,
+        url: '',
+        class: 'image-1',
+      }
+    ],
+    used: [
+      'html', 
+      'css'
+    ],
+    url: 'https://miguelgarcia95.github.io/Chamber-Of-Nature-/',
+  },
+  {
+    name: 'Kanz | Digital Agency',
+    description: "Moview Website made with moviedb",
+    imageCount: 1,
+    images: [
+      {
+        id: 1,
+        url: '',
+        class: 'image-1',
+      }
+    ],
+    used: [
+      'html', 
+      'css'
+    ],
+    url: 'https://miguelgarcia95.github.io/Design-Site/',
+  },
+  {
+    name: 'Game View v2',
+    description: "Moview Website made with moviedb",
+    imageCount: 1,
+    images: [
+      {
+        id: 1,
+        url: '',
+        class: 'image-1',
+      }
+    ],
+    used: [
+      'html', 
+      'css'
+    ],
+    url: 'https://miguelgarcia95.github.io/Game-View/#/',
+  },
 ]
