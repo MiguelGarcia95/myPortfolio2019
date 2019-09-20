8d887bf30d4dcfef01284eec4abbbd6550bfafbe
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-19 23:57:21 -0500

    create project data

--- src/projectData.js
+++ src/projectData.js
@@ -1,3 +1,19 @@
-export default {
-  data: 'sdfsdf'
-}
+export default [
+  {
+    name: '',
+    description: '',
+    imageCount: 1,
+    images: [
+      {
+        id: 1,
+        url: '',
+        class: 'image-1',
+      }
+    ],
+    used: [
+      'html', 
+      'css'
+    ],
+    url: '',
+  }
+]
