6ade7727f38c097ff7f882d2811d596b84e55bcd
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-20 18:09:50 -0500

    create project data

--- src/projectData.js
+++ src/projectData.js
@@ -21,7 +21,7 @@ export default [
   },
   {
     name: 'Draggable Planner (Trello Clone)',
-    description: "A Trello clone",
+    description: "A Trello Clone made using ReactJS, NodeJS and Redux.",
     imageCount: 1,
     images: [
       {
@@ -34,6 +34,8 @@ export default [
       'HTML/CSS', 
       'Javascript',
       'ReactJS',
+      'Redux',
+      'Ajax',
       'NodeJS',
       'Express',
       'Heroku'
@@ -41,8 +43,8 @@ export default [
     url: 'https://react-draggable-planner.herokuapp.com/#/',
   },
   {
-    name: 'Moview Search Base',
-    description: "Moview Website made with moviedb",
+    name: 'Movie Search Base',
+    description: "This project is a movie search App made using react, redux, and the TMDB API.",
     imageCount: 1,
     images: [
       {
@@ -52,14 +54,18 @@ export default [
       }
     ],
     used: [
-      'html', 
-      'css'
+      'HTML/CSS', 
+      'Javascript',
+      'ReactJS', 
+      'Redux',
+      'firebase',
+      'O-Auth'
     ],
     url: 'https://movie-searchbase.firebaseapp.com',
   },
   {
     name: 'Nike Shoe Display',
-    description: "Moview Website made with moviedb",
+    description: "Animated Shoe Display Website made using Gsap",
     imageCount: 1,
     images: [
       {
@@ -69,14 +75,16 @@ export default [
       }
     ],
     used: [
-      'html', 
-      'css'
+      'HTML/CSS', 
+      'Javascript',
+      'Gsap/Animation',
+      'Photoshop',
     ],
     url: 'https://miguelgarcia95.github.io/Shoe-Page/',
   },
   {
     name: 'Chamber of Nature - Book Promotion Site',
-    description: "Moview Website made with moviedb",
+    description: "A Book Promotion website. ",
     imageCount: 1,
     images: [
       {
@@ -86,8 +94,11 @@ export default [
       }
     ],
     used: [
-      'html', 
-      'css'
+      'HTML/CSS', 
+      'Javascript',
+      'Gsap/Animation',
+      'Scrollmagic',
+      'Photoshop',
     ],
     url: 'https://miguelgarcia95.github.io/Chamber-Of-Nature-/',
   },
@@ -103,14 +114,16 @@ export default [
       }
     ],
     used: [
-      'html', 
-      'css'
+      'HTML/CSS', 
+      'Javascript',
+      'Scrollmagic',
+      'Photoshop',
     ],
     url: 'https://miguelgarcia95.github.io/Design-Site/',
   },
   {
     name: 'Game View v2',
-    description: "Moview Website made with moviedb",
+    description: "A game database website. It includes games, game reviews, game franchises and game characters. It contains over 15,000 games. All powered by the giant bomb API, along with React JS and Redux.",
     imageCount: 1,
     images: [
       {
@@ -120,8 +133,11 @@ export default [
       }
     ],
     used: [
-      'html', 
-      'css'
+      'HTML/CSS', 
+      'Javascript',
+      'ReactJS',
+      'Redux',
+      'Ajax',
     ],
     url: 'https://miguelgarcia95.github.io/Game-View/#/',
   },
