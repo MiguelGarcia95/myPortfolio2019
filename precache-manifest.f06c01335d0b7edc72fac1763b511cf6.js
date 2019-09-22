9342729db695eb9a6f3d6107566530c4651f05f5
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-09-21 19:22:23 -0500

    Updates

---
+++ precache-manifest.f06c01335d0b7edc72fac1763b511cf6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da77dd5bf0afb70f1639d3888712d64f",
+    "url": "/myPortfolio2019/index.html"
+  },
+  {
+    "revision": "5776450821550cb92999",
+    "url": "/myPortfolio2019/static/css/main.a54fcb8d.chunk.css"
+  },
+  {
+    "revision": "57981149d9723ff512c8",
+    "url": "/myPortfolio2019/static/js/2.fd6d1a6b.chunk.js"
+  },
+  {
+    "revision": "5776450821550cb92999",
+    "url": "/myPortfolio2019/static/js/main.804eafc7.chunk.js"
+  },
+  {
+    "revision": "9c4133dbddcc1f19fc6e",
+    "url": "/myPortfolio2019/static/js/runtime~main.4b8d2cde.js"
+  }
+]);
