4a61ee37badfd01e03f237ef3322ddd9fa8d18b0
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-11-27 18:58:55 -0600

    style  navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles/css/style.min.css';
 
-function Navbar({scrollTo}) {
+function Navbar({scrollTo, opened}) {
   return (
     <div className='navbar'>
       <div className="page-container">
@@ -9,7 +9,15 @@ function Navbar({scrollTo}) {
           <p className="title">Miguel Garcia</p>
           <p className="subtitle">Web Developer</p>
         </div>
-        <div className="menu">
+        <div className={`menu ${!opened && 'closed'}`}>
+          {/* {!opened && (
+
+          )} */}
+          <div className="menu-ham">
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </div>
           <p className="menu-item">Contact</p>        
           <p className="menu-item">About</p>        
           <p className="menu-item">Skills</p>        
