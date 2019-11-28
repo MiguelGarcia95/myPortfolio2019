e7801d20527c65e3c1c1f21b677c78915ef04e85
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-11-28 02:41:59 -0600

    edit projects section

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -4,7 +4,23 @@ import './styles/css/style.min.css'
 function Projects() {
   return (
     <div className='section projects'>
-      
+      <div className="page-container">
+        <div className="header">
+          <p className="name">Miguel Garcia</p>
+        </div>
+        <div className="content">
+          <div className="header">
+            <h1 className="background-title">Projects</h1>
+            <h1 className="title">Projects</h1>
+          </div>
+
+          <div className="projects-display">
+            <div className="project">
+              
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
