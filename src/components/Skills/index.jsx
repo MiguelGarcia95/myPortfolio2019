b4257cd3b80f0e34db0a38069f610d44bef05c3a
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-11-29 15:42:03 -0600

    build skills section

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import SectionTitle from '../SectionTitle';
+import './styles/css/style.min.css';
+
+const ICON_PATH = './assets/icons';
 
 function Skills({scrollTo}) {
   return (
@@ -12,11 +15,20 @@ function Skills({scrollTo}) {
             <p>HTML, CSS, JS, ReactJS, ReduxJS for Front End websites and applications.</p>
             <p>NodeJS, Express, PHP, Laravel, MongoDB, MySQL for the Back End</p>
             <p>
-              Visit my <a className='link' href="https://github.com/MiguelGarcia95" target='_blank'>Github</a> 
-              or <p onClick={() => scrollTo('#contact')} className='link' >Contact</p> me.
+              Visit my <a className='link' href="https://github.com/MiguelGarcia95" target='_blank'>Github</a> or <span onClick={() => scrollTo('#contact')} className='link' >Contact</span> me.
             </p>
           </div>
-        </div>        
+          <div className="skills-container">
+            <div className="skill">
+              <div className="skill-icon">
+                <img src={`${ICON_PATH}/html.png`} alt='html'/>
+              </div>
+              <div className="skill-name">
+                <p>HTML</p>
+              </div>
+            </div>
+          </div> 
+        </div>       
       </div>
     </div>
   )
